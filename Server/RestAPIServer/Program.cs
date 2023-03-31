9831eefb740060475253192ae1c7e24d7846a552
Jay Gatchalian <jay.gatchalian@ext.essilor.com>
2023-03-31 17:31:23 +0800

    Initial adding of swagger options and configuration

--- Server/RestAPIServer/Program.cs
+++ Server/RestAPIServer/Program.cs
@@ -5,15 +5,37 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen( swaggerGenOptions => {
+    swaggerGenOptions.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo{
+        Version = "v1",
+        Title = "MoneyMeChallenge API",
+        Description = "",
+        TermsOfService = new Uri("https://github.com/DStickyPaws/MoneyMeChallenge/blob/RestServerCoding/TermsOfService.md"),
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact {
+            Name = "D`StickyPaws",
+            Url = new Uri("https://github.com/DStickyPaws")
+        },  
+        License = new Microsoft.OpenApi.Models.OpenApiLicense
+        {
+            Name = "License",
+            Url = new Uri("https://github.com/DStickyPaws/MoneyMeChallenge/blob/RestServerCoding/LICENSE")
+        }
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger( swaggerOptions => {
+        swaggerOptions.SerializeAsV2 = true;
+    });
+
+    app.UseSwaggerUI(swaggerUIOptions => {
+        swaggerUIOptions.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        swaggerUIOptions.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
