da8b13ea47840f24b5b0662cd66cfc4227ab6ccc
DStickyPaws <72427865+DStickyPaws@users.noreply.github.com>
2023-04-05 16:53:38 +0800

    Added Client Engine added IClient and Client Model

---
+++ Server/RestAPIServer/Engines/ClientEngine.cs
@@ -0,0 +1,90 @@
+﻿using RestAPIServer.Interface;
+using RestAPIServer.Models;
+
+namespace RestAPIServer.Engines;
+
+public class ClientEngine
+{
+    /**
+        <summary>
+            Creates an IClient based on Client.
+        </summary>
+        <param name="client">Record. The Client information. This is either made using JSON or using the backend.</param>
+        <returns>
+            IClient.
+        </returns>
+    */
+    public Task<IClient> Create(Client client)
+    {
+        IClient Result;
+
+        Result = new Client(client.FirstName,client.LastName,client.DateOfBirth, client.Id);
+
+        return Task.FromResult(Result);
+    }
+
+    /**
+        <summary>
+            Creates an IClient based on parameters provided.
+        </summary>
+        <param name="FirstName">String. The client's supposed FirstName</param>
+        <param name="LastName">String. The client's supposed LastName</param>
+        <param name="DateOfBirth">String. The client's supposed Date of Birth</param>
+        <param name="Id">Optional. Long. </param>
+        <returns>
+            IClient.
+        </returns>
+    */
+    public Task<IClient> Create(string FirstName, string LastName, string DateOfBirth, long? Id = null)
+    {
+        IClient Result;
+
+        Result = new Client(FirstName, LastName, DateOfBirth, Id);
+
+        return Task.FromResult(Result);
+    }
+    /**
+        <summary>
+        
+        </summary>
+        <param name="Client"></param>
+        <returns></returns>
+    */
+    public Task<bool> IsValid(IClient Client)
+    {
+        bool Result, isValidId, isValidFirstName, isValidLastName, isValidDateOfBirth;
+
+        isValidId = IsValidId(Client.Id).Result;
+        isValidFirstName = IsValidFirstName(Client.FirstName).Result;
+        isValidLastName = IsValidLastName(Client.LastName).Result;
+        isValidDateOfBirth = IsValidDateOfBirth(Client.DateOfBirth).Result;
+
+        Result = isValidId && isValidFirstName && isValidLastName && isValidDateOfBirth;
+
+        return Task.FromResult(Result);
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="Id"></param>
+    /// <returns></returns>
+    private Task<bool> IsValidId(long? Id) { return Task.FromResult(true); }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="FirstName"></param>
+    /// <returns></returns>
+    private Task<bool> IsValidFirstName(string FirstName) { return Task.FromResult(true); }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="LastName"></param>
+    /// <returns></returns>
+    private Task<bool> IsValidLastName(string LastName) { return Task.FromResult(true); }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="DateOfBirth"></param>
+    /// <returns></returns>
+    private Task<bool> IsValidDateOfBirth(string DateOfBirth) { return Task.FromResult(true); }
+}
--- Server/RestAPIServer/Models/Client.cs
+++ Server/RestAPIServer/Models/Client.cs
@@ -1,8 +1,28 @@
 ﻿using RestAPIServer.Interface;
+using System.Text.Json.Serialization;
 
-namespace RestAPIServer.Models
+namespace RestAPIServer.Models;
+
+public record Client : IClient
 {
-    public class Client : IClient
+    [JsonPropertyName("Id")]
+    public long? Id { get; set; }
+
+    [JsonPropertyName("FirstName")]
+    public string FirstName { get; set; }
+
+    [JsonPropertyName("LastName")]
+    public string LastName { get; set; }
+
+    [JsonPropertyName("DateOfBirth")]
+    public string DateOfBirth { get; set; }
+
+    [JsonConstructor]
+    public Client(string FirstName, string LastName, string DateOfBirth, long? Id = null)
     {
+        this.Id = Id;
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+        this.DateOfBirth = DateOfBirth;
     }
 }
